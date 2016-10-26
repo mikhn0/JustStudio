e8427083b4a8cd15c3b3272ed46a6c1ab6944323
Виктория <VaporousTransform@gmail.com>
2016-10-26 18:24:08 +0300

    upload last version with iwatch

--- JustFacts WatchOS Extension/LibraryWatchAPI.swift
+++ JustFacts WatchOS Extension/LibraryWatchAPI.swift
@@ -58,57 +58,14 @@ class LibraryWatchAPI : NSObject  {
         
         task.resume()
         
-        //----------
     }
-    
-        
-        
-        
-        
-        
-        /*Alamofire.request("\(SERVER_URL)/categories")
-            .validate(statusCode: 200..<300)
-            .validate(contentType: ["application/json"])
-            .responseJSON { response in
-                
-                print(response.request)  // original URL request
-                print(response.response) // HTTP URL response
-                print(response.data)     // server data
-                print(response.result)   // result of response serialization
-                
-                if response.result.value != nil {
-                    switch response.result {
-                    case .success(let JSON):
-                        
-                        let jsonDict = JSON as! NSDictionary
-                        let jsonCategoryArr = jsonDict["categories"] as! [AnyObject]
-                        
-                        var categoryArr: [Category] = []
-                        let categories = jsonCategoryArr.shuffle()
-                        for element in categories {
-                            let dict = element as! [String : AnyObject]
-                            let categoryData = Category(category: dict)
-                            categoryArr.append(categoryData)
-                            
-                        }
-                        completion(categoryArr)
-                        
-                        
-                    case .failure(let error):
-                        print("Request failed with error: \(error)")
-                        completion([])
-                        
-                    }
-                }
-            }*/
-    }
-    
+}
 
-    /////
+
 
     func getFactsByCategoryForWatch(_ category:String, completion:@escaping([Fact]) -> Void) -> Void {
        //-----
-        let url = NSURL(string: "\(SERVER_URL)/facts?category=\(category)")//\(SERVER_URL)/facts", parameters:["category":category]
+        let url = NSURL(string: "\(SERVER_URL)/facts?category=\(category)")
         
         let task = URLSession.shared.dataTask(with: url! as URL) {(data, response, error) in
             print(NSString(data: data!, encoding: String.Encoding.utf8.rawValue))
@@ -139,35 +96,7 @@ class LibraryWatchAPI : NSObject  {
         
         task.resume()
 }
-        //-----
-        
-        
-        
-        
-        
-        /*Alamofire.request("\(SERVER_URL)/facts", parameters:["category":category])
-            .validate(statusCode: 200..<300)
-            .validate(contentType: ["application/json"])
-            .responseJSON { response in
-                
-                let jsonDict = response.result.value as! NSDictionary
-                let jsonFactsArr = jsonDict["facts"] as! NSArray
-                
-                var MyfactsArr: [Fact] = []
-                for element in jsonFactsArr {
-                    let dict = element as! NSDictionary
-                    let factObject = Fact(id:(dict["_id"] as? String) , active:(dict["active"] as? Bool), ru:(dict["ru"] as? String), image:(dict["image"] as? String), en:(dict["en"] as? String) ,category:(dict["category"] as? String))
-                    MyfactsArr.append(factObject)
-                }
-                
-                completion(MyfactsArr)
-                
-        }*/
 
-
-
-
-/////
 
 extension Collection {
     /// Return a copy of `self` with its elements shuffled
